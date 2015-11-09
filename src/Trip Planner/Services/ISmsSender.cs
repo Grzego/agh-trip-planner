11eeff42866c70b2c0bfd23ee84a6a60fc4fe56a
Grzegorz Opoka <grzego.opoka@live.com>
2015-11-09 13:25:12 +0100

    Init commit.

---
+++ src/Trip Planner/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trip_Planner.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
