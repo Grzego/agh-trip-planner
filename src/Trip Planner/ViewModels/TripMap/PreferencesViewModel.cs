fb425e428ec7c28863987ed5110f731a15e9cb9d
Lukasz <lukasz14076@outlook.com>
2015-12-04 20:24:34 +0100

    Preference i TripMap

---
+++ src/Trip Planner/ViewModels/TripMap/PreferencesViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Trip_Planner.ViewModels.TripMap
+{
+    public class PreferencesViewModel
+    {
+        [Display(Name = "Miasto")]
+        public String City { get; set; }
+        [Display(Name = "Kluby")]
+        public bool Club { get; set; }
+        [Display(Name = "Zabytki")]
+        public bool Monuments { get; set; }
+        [Display(Name = "Data przyjazdu")]
+        public DataType BeginTrip { get; set; }
+        [Display(Name = "Data wyjazdu")]
+        public DataType EndTrip { get; set; }
+    }
+}
