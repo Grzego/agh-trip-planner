c0f6cacc23e0fedb9cc171b3787aa588399bb4c7
Grzegorz Opoka <grzego.opoka@live.com>
2015-12-06 17:17:49 +0100

    starting with asp tags for checkbox

---
+++ src/Trip Planner/TagHelpers/CheckboxTagHelper.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNet.Razor.Runtime.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trip_Planner.TagHelpers
+{
+	[HtmlTargetElement("div", Attributes = CheckboxValueAttributeName)]
+	public class CheckboxTagHelper : TagHelper
+	{
+		private const string CheckboxValueAttributeName = "my-checkbox-for";
+
+		[HtmlAttributeName(CheckboxValueAttributeName)]
+		public string CheckboxName { get; set; }
+
+		public override void Process(TagHelperContext context, TagHelperOutput output)
+		{
+			string checkboxContent = $@"<input asp-for='{CheckboxName}' id='{CheckboxName}' type='checkbox' class='filled-in'/>
+										<label for='{CheckboxName}'>{CheckboxName}</label>";
+
+			output.Content.Append(checkboxContent);
+
+			base.Process(context, output);
+		}
+	}
+}
